9fdac6ed23d795b45ac3ef8387cd355ac0c00da3
Bryan <morganthemosaic@gmail.com>
2023-04-15 20:20:33 -0400

    Display current weather condition

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-
-const longitude = document.querySelector("#longitude");
-const latitude = document.querySelector("#latitude");
+import { keys } from "./keys.js";
+const openWeatherMap = keys.openWeatherMap;
+const latitudeInput = document.querySelector("#latitude");
+const longitudeInput = document.querySelector("#longitude");
 const submitButton = document.querySelector(".submit");
 
 submitButton.addEventListener("click", () => {
-    console.log(longitude.value, latitude.value);
-})
+	const longitude = longitudeInput.value;
+	const latitude = latitudeInput.value;
+	fetch(
+		`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherMap}`
+	)
+		.then((response) => response.json())
+		.then((weatherData) => {
+			weatherDeclaration.textContent +=
+				weatherData.current.weather[0].description;
+		});
+});
+
+let weatherDeclaration = document.querySelector(".weather");
