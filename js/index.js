eab5a0614aaa21bea5185f01b7af6c464e0e61e1
Bryan <morganthemosaic@gmail.com>
2023-04-18 20:16:50 -0400

    Refactored code to use async/await

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
 import { keys } from "./keys.js";
+
 const openWeatherMap = keys.openWeatherMap;
 const latitudeInput = document.querySelector("#latitude");
 const longitudeInput = document.querySelector("#longitude");
 const submitButton = document.querySelector(".submit");
+const weatherDeclaration = document.querySelector(".weather");
 
-submitButton.addEventListener("click", () => {
+const getWeatherDescription = async (latitude, longitude) => {
+	const response = await fetch(
+		`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherMap}`
+	);
+	const weatherData = await response.json();
+	const weatherDescription = await weatherData.current.weather[0].description;
+
+	return weatherDescription;
+};
+
+submitButton.addEventListener("click", async () => {
 	const longitude = longitudeInput.value;
 	const latitude = latitudeInput.value;
-	fetch(
-		`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherMap}`
-	)
-		.then((response) => response.json())
-		.then((weatherData) => {
-			weatherDeclaration.textContent +=
-				weatherData.current.weather[0].description;
-		});
-});
+	const weatherDescription = await getWeatherDescription(latitude, longitude);
 
-let weatherDeclaration = document.querySelector(".weather");
+	weatherDeclaration.textContent += ` ${weatherDescription}`;
+});
